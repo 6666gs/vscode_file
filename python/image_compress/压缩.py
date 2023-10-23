a193e8a64a56148882ed93922796990329399782
office_computer <wux6231@gmail.com>
2023-10-23 13:50:59 +0800

    增加了一个压缩图片的脚本

---
+++ python/image_compress/压缩.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+
+def compress_image(infile, outfile='', mb=1500, step=10, quality=80):
+    """不改变图片尺寸压缩到指定大小
+    :param infile: 压缩源文件
+    :param outfile: 压缩文件保存地址
+    :param mb: 压缩目标，以kb为单位
+    :param step: 每次调整的压缩比率
+    :param quality: 初始压缩比率
+    :return: 压缩文件地址，压缩文件大小
+    """
+    o_size = os.path.getsize(infile)/1024
+    if o_size <= mb:
+        im = Image.open(infile)
+        im.save(outfile)
+    while o_size > mb:
+        im = Image.open(infile)
+        im.save(outfile, quality=quality)
+        if quality - step < 0:
+            break
+        quality -= step
+        o_size = os.path.getsize(outfile)/1024
+    return outfile, os.path.getsize(outfile)/1024        
+
+infile_path="D:/vscode_file/vscode_file/python/image_compress/infile"           #输入图片文件夹
+outfile_path="D:/vscode_file/vscode_file/python/image_compress/outfile"         #输出图片文件夹
+files=os.listdir(infile_path)                           #得到指定文件夹中全部文件名（默认没有子文件夹）
+print(files)
+for num,file in enumerate(files):
+    infile=infile_path+'/'+file
+    outfile=outfile_path+'/'+file
+    a,b=compress_image(infile,outfile)
+    print(num+1,':',a,'    ',b)
